6b968561dd6449cbb86ea8b2c11d27c8469dd3c3
Vinícius Meneses <viniciusmenesesdev@gmail.com>
2024-11-11 12:59:57 -0300

    first commit

---
+++ src/main/java/com/pl/premier_zone/PremizerZoneApplication.java
@@ -0,0 +1,13 @@
+package com.pl.premier_zone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremizerZoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremizerZoneApplication.class, args);
+	}
+
+}
